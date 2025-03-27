2c2458d45348d6c0dabb5879377d38d8882caad5
Depak Borhara <depakborhara@gmail.com>
2025-03-27 15:01:10 -0400

    Remove submodules eza and nvim

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,27 @@
+local wezterm = require("wezterm")
+
+return {
+	color_scheme = "Tokyo Night",
+	default_prog = { "/bin/zsh", "-l" }, -- -l = login shell
+	font_size = 16.0,
+	font = wezterm.font("JetBrainsMono Nerd Font"),
+	hide_tab_bar_if_only_one_tab = true,
+	enable_scroll_bar = false,
+	use_fancy_tab_bar = false,
+	window_padding = {
+		left = 8,
+		right = 8,
+		top = 6,
+		bottom = 6,
+	},
+	keys = {
+		{
+			key = "n",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.SendString("nvim\n"),
+		},
+	},
+	set_environment_variables = {
+		WEZTERM_ACTIVE = "1",
+	},
+}
